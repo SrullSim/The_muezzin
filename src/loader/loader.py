85b55eb60b913291b3af987182738aa60399df1d
srullsim <simchonisrael@gmail.com>
2025-09-07 12:57:20 +0300

    added some functions on loader

--- src/loader/loader.py
+++ src/loader/loader.py
@@ -1,10 +1,8 @@
 import datetime
 import struct
-
 from config.config import PROJECT_ROOT,DATA_DIR
 from pathlib import Path
 import os
-from scipy.io import wavfile
 
 
 
@@ -25,26 +23,29 @@ class Loader:
         return wav_data_list
 
 
-
     def get_metadata_on_file(self, file) -> object:
         try:
             metadata = os.stat(file)
-            print(metadata.st_size)
-            return metadata
+            # print(metadata.st_size)
+            data_file = {file: metadata}
+            return data_file
         except FileNotFoundError:
             print(f"Error: File not found at {file}")
 
 
     def read_metadata(self):
         wav_files = self.read_files_to_list(self.path_to_data_dir)
-
+        data_to_publish = []
         for file in wav_files:
-            metadata = self.get_metadata_on_file(file)
+            dict_metadata = self.get_metadata_on_file(file)
+            data_to_publish.append(dict_metadata)
+
+        return data_to_publish
 
 
 
 
 
-
-# l = Loader()
-# l.read_metadata()
+l = Loader()
+d =l.read_metadata()
+print(d)
